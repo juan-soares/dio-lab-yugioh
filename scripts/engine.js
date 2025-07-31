1f4aab698635ad515956ee91d5e6d393ce6d9f1c
juan-soares <juan.css013@gmail.com>
2025-07-31 20:37:39 -0300

    feat: bgm

--- scripts/engine.js
+++ scripts/engine.js
@@ -187,6 +187,9 @@ function init() {
   showHiddenCardFieldsImages(false);
   drawCards(5, playerSides.player1);
   drawCards(5, playerSides.computer);
+
+  const bgm = document.getElementById("bgm");
+  bgm.play();
 }
 
 init();
